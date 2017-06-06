d930233de5125f9bc1418e5ecb0290ce33604e53
eTingz <etingleff@gmail.com>
2017-06-05 21:34:01 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $("#linkedin").on("click",function(){
+    window.open("https://www.linkedin.com/in/eric-tingleff");
+  });
+  $("#github").on("click", function(){
+    window.open("https://github.com/eTingz");
+  });
+  $("#fcc").on("click", function(){
+    window.open("https://www.freecodecamp.com/etingz");
+  });
+  $("#codepen").on("click", function(){
+    window.open("https://codepen.io/eTingz/");
+  });
+  $("#twitter").on("click", function(){
+    window.open("https://twitter.com/e_Tingz");
+  });
+  
+});
